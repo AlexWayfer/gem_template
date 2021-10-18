2c272944356ceaf8754d4759cbdb3ccb388693c8
Alexander Popov <alex.wayfer@gmail.com>
2021-10-18 09:18:52 +0300

    Add specs from template and fix issues

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+
+RSpec::Matchers.define :include_lines do |expected_lines|
+	match do |actual_text|
+		expected_lines.all? do |expected_line|
+			actual_text.lines.any? do |actual_line|
+				actual_line.strip == expected_line.strip
+			end
+		end
+	end
+
+	diffable
+end
+
+RSpec::Matchers.define_negated_matcher :not_ending_with, :ending_with
+RSpec::Matchers.define_negated_matcher :not_include, :include
+RSpec::Matchers.define_negated_matcher :not_include_lines, :include_lines
+RSpec::Matchers.define_negated_matcher :not_match, :match
+RSpec::Matchers.define_negated_matcher :not_output, :output
